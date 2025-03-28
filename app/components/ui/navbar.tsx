adeb15490f6aae0c34130dc95b9ec3b141f89f07
Bhee-tec <thebheetv@gmail.com>
2025-03-28 21:13:30 +0100

    Update navbar.tsx

--- app/components/ui/navbar.tsx
+++ app/components/ui/navbar.tsx
@@ -1,7 +1,6 @@
 "use client";
 import {useRouter} from 'next/navigation';
 import React from 'react';
-import Link from 'next/link';
 
 export default function navbar() {
     const router = useRouter();
