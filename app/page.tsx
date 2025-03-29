19a34cfb05380959b841797e5bd169b93a305163
Bhee-tec <thebheetv@gmail.com>
2025-03-29 17:45:50 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import Navbar from "@/app/components/ui/navbar",
-import Header from '@/app/components/ui/header'
-import Game from '@/app/components/ui/game'
-import Gamedata from '@/app/components/ui/gamedata'
+import Navbar from '@/app/components/ui/navbar';
+import Header from '@/app/components/ui/header';
+import Game from '@/app/components/ui/game';
+import Gamedata from '@/app/components/ui/gamedata';
 export default function page() {
   return (
     <div className="flex flex-col min-h-screen  bg-[#1a1c2e] text-white p-4 font-sans">
